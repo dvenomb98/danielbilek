a061f6eff20edf5ce6ccb0c190d3f846affc75c3
Daniel Bílek <daniel.bilek@regiojet.com>
2023-07-27 15:52:32 +0200

    added observer for technologies page

--- pages/technologies.tsx
+++ pages/technologies.tsx
@@ -1,9 +1,37 @@
 import PageHeader from '@/components/atoms/PageHeader';
 import PageLayout from '@/components/layouts/PageLayout';
 import data from '@/data/technologies/technologies';
+import classNames from 'classnames';
 import { NextPage } from 'next';
 import { NextSeo } from 'next-seo';
-import React from 'react';
+import React, { FC } from 'react';
+import { useInView } from 'react-intersection-observer';
+
+interface TechnologiesListProps {
+  data: {
+    name: string;
+    description: string;
+  };
+}
+
+const TechnologiesList: FC<TechnologiesListProps> = ({ data: { name, description } }) => {
+  const { ref, inView } = useInView({
+    delay: 150,
+  });
+
+  return (
+    <div
+      ref={ref}
+      className={classNames(
+        inView ? 'opacity-100 scale-100' : 'opacity-20 scale-90',
+        'transition-all duration-500 ease-in-out',
+      )}
+    >
+      <li className="font-bold text-h3">{name}</li>
+      <span className="text-secondary">{description}</span>
+    </div>
+  );
+};
 
 const Technologies: NextPage = () => {
   return (
@@ -20,10 +48,7 @@ const Technologies: NextPage = () => {
 
         <ul className="flex flex-col gap-8">
           {data.map(({ name, description }) => (
-            <div key={name}>
-              <li className="font-bold text-h3">{name}</li>
-              <span className="text-secondary">{description}</span>
-            </div>
+            <TechnologiesList key={name} data={{ name, description }} />
           ))}
         </ul>
       </PageLayout>
